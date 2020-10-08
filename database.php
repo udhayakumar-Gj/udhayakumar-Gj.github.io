a4f66a5fe4ab9b38b7af8ef925b3cbefb78cb0de
udhayakumar-Gj <56712688+udhayakumar-Gj@users.noreply.github.com>
2020-10-08 12:16:14 +0530

    Delete database.php

--- database.php
+++
@@ -1,8 +0,0 @@
-<?php 
- $conn=mysqli_connect('localhost','root','Udhaya@123','company');
- if(mysqli_connect_errno($conn)){
-	echo 'Failed to connect to database:'.mysqli_connect_error;
-  }else {
-    echo'connected';
-  }
-?>  
